ac962d9e520a4f192e06f50d2fe102927ceaef14
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-04-26 09:55:00 +0300

    Init project

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        val butSearch: Button = findViewById<Button>(R.id.search)
+        val butSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            } }
+        butSearch.setOnClickListener(butSearchClickListener)
+
+        val butLib = findViewById<Button>(R.id.library)
+        butLib.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+        }
+        val butSettings = findViewById<Button>(R.id.settings)
+        butSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+        }
 
     }
+
+
 }
