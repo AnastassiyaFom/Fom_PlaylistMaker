57d7573e31224a7b79c3069df87f97feee6b03d2
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-04-28 21:50:17 +0300

    HomeWork Sprint 8

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,36 +1,37 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        val butSearch: Button = findViewById<Button>(R.id.search)
+        val butSearch = findViewById<Button>(R.id.search)
         val butSearchClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
-            } }
+                val displaySearchActivity = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(displaySearchActivity)
+            }
+        }
         butSearch.setOnClickListener(butSearchClickListener)
 
         val butLib = findViewById<Button>(R.id.library)
         butLib.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            val displayLibraryActivity = Intent(this, LibraryActivity::class.java)
+            startActivity(displayLibraryActivity)
         }
+
         val butSettings = findViewById<Button>(R.id.settings)
         butSettings.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            val displaySettingsActivity = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySettingsActivity)
         }
 
+
     }
-
-
 }
